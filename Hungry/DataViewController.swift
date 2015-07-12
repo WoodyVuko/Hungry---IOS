e9a6fd67ef49ae77ea6179d757115f8ea931e16c
Goran Vukovic <TheRealVuko@gmx.de>
2015-07-12 22:09:26 +0200

    Facebook Login funktioniert, ID wird herausgefunden

---
+++ Hungry/DataViewController.swift
@@ -0,0 +1,77 @@
+//
+//  DataViewController.swift
+//  Hungry
+//
+//  Created by Goran Vukovic on 12.07.15.
+//  Copyright © 2015 Goran Vukovic. All rights reserved.
+//
+import FBSDKLoginKit
+import FBSDKCoreKit
+
+import UIKit
+
+class DataViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    @IBOutlet var name: FBSDKProfile!
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    var dataObject: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() == nil)
+        {
+            print("Not logged in..")
+        }
+        else
+        {
+            print("Logged in..")
+            print("\(FBSDKAccessToken.currentAccessToken().userID)")
+            //print("\(FBSDKProfile.currentProfile().name)")
+
+            //self.performSegueWithIdentifier("LoggedIn", sender: self)
+        }
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+    // MARK: - Facebook Login
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+    {
+        if error == nil
+        {
+            print("Login complete.")
+            self.performSegueWithIdentifier("showFB_Login", sender: self)
+        }
+        else
+        {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        print("User logged out...")
+    }
+    
+}
